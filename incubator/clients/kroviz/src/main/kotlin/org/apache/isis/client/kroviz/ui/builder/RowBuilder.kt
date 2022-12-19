c5a904d41f66b8d4e134c43eade1fe9aa73aed6c
Supermajestcd <118209770+Supermajestcd@users.noreply.github.com>
2022-12-19 06:17:37 -0700

    Initialize ISIS-2801

---
+++ incubator/clients/kroviz/src/main/kotlin/org/apache/isis/client/kroviz/ui/builder/RowBuilder.kt
@@ -0,0 +1,63 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.client.kroviz.ui.builder
+
+import org.apache.isis.client.kroviz.to.TObject
+import org.apache.isis.client.kroviz.to.bs3.Row
+import org.apache.isis.client.kroviz.ui.core.MenuFactory
+import org.apache.isis.client.kroviz.ui.core.RoDisplay
+import io.kvision.core.*
+import io.kvision.core.FlexWrap
+import io.kvision.panel.*
+
+class RowBuilder {
+
+    fun create(row: Row, tObject: TObject, dsp: RoDisplay): SimplePanel {
+        val result = FlexPanel(
+                FlexDirection.ROW,
+                FlexWrap.NOWRAP,
+                JustifyContent.FLEXSTART,
+                AlignItems.FLEXSTART,
+                AlignContent.STRETCH,
+                spacing = 10 )
+
+        for (c in row.colList) {
+            val cpt = ColBuilder().create(c, tObject, dsp)
+            result.add(cpt)
+        }
+        return result
+    }
+
+    fun createMenu(tObject: TObject, dsp: RoDisplay): HPanel {
+        val result = HPanel()
+        result.width = CssSize(100, UNIT.perc)
+        result.height = CssSize(100, UNIT.perc)
+
+        val dd = MenuFactory.buildForObject(tObject)
+        dd.marginTop = CssSize(10, UNIT.px)
+        dd.marginBottom = CssSize(10, UNIT.px)
+        MenuFactory.amendWithSaveUndo(dd, tObject)
+        MenuFactory.disableSaveUndo(dd)
+        dsp.menu = dd
+        result.add(dd)
+
+        return result
+    }
+
+}
---
+++ security/spring/src/main/java/org/apache/isis/security/spring/webmodule/SpringSecurityFilter.java
@@ -0,0 +1,93 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.security.spring.webmodule;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.apache.isis.applib.services.iactnlayer.InteractionContext;
+import org.apache.isis.applib.services.iactnlayer.InteractionService;
+import org.apache.isis.applib.services.user.UserMemento;
+import org.apache.isis.applib.services.user.UserMemento.AuthenticationSource;
+import org.apache.isis.security.spring.authconverters.AuthenticationConverter;
+
+import lombok.val;
+
+/**
+ * @since 2.0 {@index}
+ */
+//@Log4j2
+public class SpringSecurityFilter implements Filter {
+
+    @Autowired private InteractionService interactionService;
+
+    @Override
+    public void doFilter(
+            final ServletRequest servletRequest,
+            final ServletResponse servletResponse,
+            final FilterChain filterChain) throws IOException, ServletException {
+
+        val httpServletResponse = (HttpServletResponse) servletResponse;
+
+        val springAuthentication = SecurityContextHolder.getContext().getAuthentication();
+        if(springAuthentication==null
+                || !springAuthentication.isAuthenticated()) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return; // not authenticated
+        }
+
+        UserMemento userMemento = null;
+        for (AuthenticationConverter converter : converters) {
+            try {
+                userMemento = converter.convert(springAuthentication);
+                if(userMemento != null) {
+                    break;
+                }
+            } catch(Exception ex) {
+                continue;
+            }
+        }
+
+        if (userMemento == null) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return; // unknown principal type, not handled
+        }
+
+        // TODO: this should be added by Wicket viewer
+        userMemento = userMemento.withRoleAdded("org.apache.isis.viewer.wicket.roles.USER")
+                .withAuthenticationSource(AuthenticationSource.EXTERNAL);
+
+        interactionService.run(
+                InteractionContext.ofUserWithSystemDefaults(userMemento),
+                ()->filterChain.doFilter(servletRequest, servletResponse));
+    }
+
+    @Inject List<AuthenticationConverter> converters;
+}
