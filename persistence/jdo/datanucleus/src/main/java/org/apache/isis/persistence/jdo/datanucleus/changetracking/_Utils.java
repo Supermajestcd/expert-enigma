fb0d37bde7ab3744045da6e38dbede8d04465ba5
Supermajestcd <118209770+Supermajestcd@users.noreply.github.com>
2022-12-19 06:25:22 -0700

    Initialize ISIS-3195

---
+++ persistence/jdo/datanucleus/src/main/java/org/apache/isis/persistence/jdo/datanucleus/changetracking/_Utils.java
@@ -0,0 +1,120 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.persistence.jdo.datanucleus.changetracking;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.ObjectState;
+import javax.jdo.listener.InstanceLifecycleEvent;
+
+import org.datanucleus.enhancement.Persistable;
+import org.springframework.lang.Nullable;
+
+import org.apache.isis.commons.internal.assertions._Assert;
+import org.apache.isis.core.metamodel.context.MetaModelContext;
+import org.apache.isis.core.metamodel.object.ManagedObject;
+import org.apache.isis.core.metamodel.object.ManagedObjects;
+import org.apache.isis.core.metamodel.objectmanager.ObjectManager.EntityAdaptingMode;
+
+import lombok.NonNull;
+import lombok.val;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+final class _Utils {
+
+    Persistable persistableFor(final InstanceLifecycleEvent event) {
+        return (Persistable)event.getPersistentInstance();
+    }
+
+    void resolveInjectionPoints(
+            final @NonNull MetaModelContext mmc,
+            final @NonNull InstanceLifecycleEvent event) {
+        final Persistable pojo = _Utils.persistableFor(event);
+        if(pojo!=null) {
+            mmc.getServiceInjector().injectServicesInto(pojo);
+        }
+    }
+
+    String debug(final InstanceLifecycleEvent event) {
+        // try to be side-effect free here ...
+        final Persistable pojo = _Utils.persistableFor(event);
+        ObjectState state = JDOHelper.getObjectState(pojo);
+        //if(state == ObjectState.PERSISTENT_CLEAN) {
+            //return String.format("entity: %s", pojo);
+        //} else {
+            return String.format("entity: %s (%s)", pojo.getClass().getSimpleName(), state);
+        //}
+    }
+
+    ManagedObject adaptEntity(
+            final @NonNull MetaModelContext mmc,
+            final @NonNull Object entityPojo,
+            final @NonNull EntityAdaptingMode bookmarking) {
+
+        val objectManager = mmc.getObjectManager();
+        val entity = objectManager.adapt(entityPojo, bookmarking);
+        _Assert.assertTrue(entity.getSpecification().isEntity());
+        return entity;
+    }
+
+    ManagedObject adaptNullableEntity(
+            final @NonNull MetaModelContext mmc,
+            final @Nullable Object entityPojo,
+            final @NonNull EntityAdaptingMode bookmarking) {
+
+        return entityPojo == null
+                ? ManagedObject.unspecified()
+                : adaptEntity(mmc, entityPojo, bookmarking);
+    }
+
+    ManagedObject adaptNullableAndInjectServices(
+            final @NonNull MetaModelContext mmc,
+            final @Nullable Object entityPojo,
+            final @NonNull EntityAdaptingMode bookmarking) {
+
+        return entityPojo == null
+                ? ManagedObject.unspecified()
+                : adaptEntityAndInjectServices(mmc, entityPojo, bookmarking);
+    }
+
+    ManagedObject adaptEntityAndInjectServices(
+            final @NonNull MetaModelContext mmc,
+            final @NonNull Object entityPojo,
+            final @NonNull EntityAdaptingMode bookmarking) {
+        return injectServices(mmc, adaptEntity(mmc, entityPojo, bookmarking));
+    }
+
+
+    private static ManagedObject injectServices(
+            final @NonNull MetaModelContext mmc,
+            final @NonNull ManagedObject adapter) {
+
+        if(ManagedObjects.isNullOrUnspecifiedOrEmpty(adapter)) {
+            return adapter;
+        }
+
+        if(adapter.getSpecification().isValue()) {
+            return adapter; // guard against value objects
+        }
+        mmc.getServiceInjector().injectServicesInto(adapter.getPojo());
+        return adapter;
+    }
+
+
+}
---
+++ viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/scalars/ScalarPanelSelectAbstract.java
@@ -0,0 +1,145 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.viewer.wicket.ui.components.scalars;
+
+import java.util.Optional;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.springframework.lang.Nullable;
+import org.wicketstuff.select2.Settings;
+
+import org.apache.isis.core.metamodel.object.ManagedObject;
+import org.apache.isis.core.metamodel.util.Facets;
+import org.apache.isis.viewer.commons.model.feature.ParameterUiModel;
+import org.apache.isis.viewer.wicket.model.models.ScalarModel;
+import org.apache.isis.viewer.wicket.ui.components.widgets.select2.Select2;
+import org.apache.isis.viewer.wicket.ui.components.widgets.select2.providers.ChoiceProviderAbstract;
+import org.apache.isis.viewer.wicket.ui.util.Wkt;
+import org.apache.isis.viewer.wicket.ui.util.Wkt.EventTopic;
+
+import lombok.NonNull;
+import lombok.val;
+
+public abstract class ScalarPanelSelectAbstract
+extends ScalarPanelFormFieldAbstract<ManagedObject> {
+
+    private static final long serialVersionUID = 1L;
+
+    protected Select2 select2;
+
+    public ScalarPanelSelectAbstract(final String id, final ScalarModel scalarModel) {
+        super(id, scalarModel, ManagedObject.class);
+        setOutputMarkupId(true);
+    }
+
+    protected final Select2 createSelect2(final String id) {
+        val select2 = Select2.createSelect2(id, scalarModel(), buildChoiceProvider());
+        addSelect2Semantics(select2.getSettings());
+        return select2;
+    }
+
+    private void addSelect2Semantics(final Settings settings) {
+        val scalarModel = scalarModel();
+
+        switch(scalarModel.getChoiceProviderSort()) {
+        case CHOICES:
+            settings.setPlaceholder(scalarModel.getFriendlyName());
+            return;
+        case AUTO_COMPLETE:
+            settings.setPlaceholder(scalarModel.getFriendlyName());
+            settings.setMinimumInputLength(scalarModel.getAutoCompleteMinLength());
+            return;
+        case OBJECT_AUTO_COMPLETE:
+            //TODO render object place holder?
+            Facets.autoCompleteMinLength(scalarModel.getScalarTypeSpec())
+            .ifPresent(settings::setMinimumInputLength);
+            return;
+        default:
+            // ignore if no choices
+        }
+    }
+
+    public final boolean checkSelect2Required() {
+        return select2.checkRequired();
+    }
+
+    /**
+     * Mandatory hook (is called by {@link #createSelect2(String)})
+     */
+    protected abstract ChoiceProviderAbstract buildChoiceProvider();
+
+    // //////////////////////////////////////
+
+    /**
+     * Automatically "opens" the select2.
+     */
+    @Override
+    protected void onSwitchFormForInlinePrompt(
+            final WebMarkupContainer inlinePromptForm,
+            final AjaxRequestTarget target) {
+        Wkt.javaScriptAdd(target, EventTopic.OPEN_SELECT2, inlinePromptForm.getMarkupId());
+    }
+
+    // //////////////////////////////////////
+
+    /**
+     * Hook method to refresh choices when changing.
+     *
+     * <p>
+     * called from onUpdate callback
+     */
+    @Override
+    public Repaint updateIfNecessary(
+            final @NonNull ParameterUiModel paramModel,
+            final @NonNull Optional<AjaxRequestTarget> target) {
+
+        val repaint = super.updateIfNecessary(paramModel, target);
+        final boolean choicesUpdated = updateChoices(this.select2);
+
+        if (repaint == Repaint.NOTHING) {
+            if (choicesUpdated) {
+                return Repaint.PARAM_ONLY;
+            } else {
+                return Repaint.NOTHING;
+            }
+        } else {
+            return repaint;
+        }
+    }
+
+    private boolean updateChoices(final @Nullable Select2 select2) {
+        if (select2 == null) {
+            return false;
+        }
+        select2.rebuildChoiceProvider();
+        return true;
+    }
+
+    /**
+     * Repaints just the Select2 component
+     *
+     * @param target The Ajax request handler
+     */
+    @Override
+    public void repaint(final AjaxRequestTarget target) {
+        target.add(this);
+    }
+
+}
