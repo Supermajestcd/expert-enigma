815c7cf1894b270da2e7ffa4d95b788b0875e2da
Supermajestcd <118209770+Supermajestcd@users.noreply.github.com>
2022-12-19 06:11:20 -0700

    Initialize ISIS-2336

---
+++ core/runtime/src/main/java/org/apache/isis/core/runtime/session/IsisSession.java
@@ -0,0 +1,119 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.isis.core.runtime.session;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.isis.applib.services.xactn.TransactionId;
+import org.apache.isis.applib.services.xactn.TransactionState;
+import org.apache.isis.core.commons.collections.Can;
+import org.apache.isis.core.commons.internal.base._Casts;
+import org.apache.isis.core.metamodel.commons.ToString;
+import org.apache.isis.core.metamodel.context.MetaModelContext;
+import org.apache.isis.core.runtime.context.session.RuntimeContextBase;
+import org.apache.isis.core.runtime.persistence.session.PersistenceSession;
+import org.apache.isis.core.security.authentication.AuthenticationSession;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+/**
+ * Holds the current set of components for a specific execution context (such as on a thread).
+ *
+ * @see IsisSessionFactory
+ */
+public class IsisSession extends RuntimeContextBase {
+
+    @Getter private final AuthenticationSession authenticationSession;
+
+    /**
+     * Set to System.nanoTime() when session opens.
+     * @deprecated use command timestamp instead
+     */
+    @Getter private long openedAtSystemNanos = -1L;
+
+    public IsisSession(
+            @NonNull final MetaModelContext mmc,
+            @NonNull final AuthenticationSession authenticationSession) {
+
+        super(mmc);
+        this.authenticationSession = authenticationSession; // binds this session to given authenticationSession
+        
+        openedAtSystemNanos = System.nanoTime();
+    }
+
+//    // -- CURRENT
+//
+//    public static IsisSession currentOrElseNull() {
+//        return current().orElse(null);
+//    }
+//
+//    public static Optional<IsisSession> current() {
+//        return _Context.threadLocalGet(IsisSession.class)
+//                .getSingleton();
+//    }
+//
+//    public static boolean isInSession() {
+//        return currentOrElseNull() != null;
+//    }
+
+
+    // -- FLUSH
+    //    void flush() {
+    //    	runtimeEventService.fireSessionFlushing(this);
+    //    }
+
+    // -- TRANSACTION
+
+    public TransactionId getCurrentTransactionId() {
+        return transactionService.currentTransactionId();
+    }
+
+    public TransactionState getCurrentTransactionState() {
+        return transactionService.currentTransactionState();
+    }
+
+
+    // -- toString
+    @Override
+    public String toString() {
+        final ToString asString = new ToString(this);
+        asString.append("persistenceSession", PersistenceSession.current(PersistenceSession.class));
+        asString.append("transaction", getCurrentTransactionId());
+        return asString.toString();
+    }
+
+
+    private final Map<Class, Can> map = new HashMap<>();
+
+    public <T> Runnable put(Class<? super T> type, T variant) {
+        map
+                .compute(type, (k, v) -> v == null
+                        ? Can.<T>ofSingleton(variant)
+                        : Can.<T>concat(_Casts.uncheckedCast(v), variant));
+
+        return () -> map.remove(type);
+    }
+
+    public <T> Can<T> get(Class<? super T> type) {
+        return map.get(type);
+    }
+}
---
+++ viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/widgets/select2/providers/ObjectAdapterMementoProviderForValueChoices.java
@@ -0,0 +1,66 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.viewer.wicket.ui.components.widgets.select2.providers;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.apache.isis.core.commons.internal.collections._Lists;
+import org.apache.isis.viewer.wicket.model.models.ScalarModel;
+import org.apache.isis.core.webapp.context.memento.ObjectMemento;
+
+import lombok.Getter;
+import lombok.val;
+
+public class ObjectAdapterMementoProviderForValueChoices
+extends ObjectAdapterMementoProviderAbstract 
+implements ObjectAdapterMementoProviderForChoices {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Getter(onMethod = @__(@Override))
+    private final List<ObjectMemento> choiceMementos;
+
+    public ObjectAdapterMementoProviderForValueChoices(
+            ScalarModel scalarModel,
+            List<ObjectMemento> choicesMementos) {
+        
+        super(scalarModel);
+        this.choiceMementos = choicesMementos;
+    }
+
+    @Override
+    protected List<ObjectMemento> obtainMementos(String term) {
+        return obtainMementos(term, choiceMementos);
+    }
+
+    @Override
+    public Collection<ObjectMemento> toChoices(final Collection<String> ids) {
+        final List<ObjectMemento> mementos = obtainMementos(null);
+
+        final Predicate<ObjectMemento> lookupOam = (ObjectMemento input) -> {
+            val id = getIdValue(input);
+            return ids.contains(id);
+        };
+        
+        return _Lists.filter(mementos, lookupOam);
+    }
+
+}
